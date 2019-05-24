c50c06aab60ce57288947f2e9e6536a73ecc7d13
Biel Borras Serra <bielet40@gmail.com>
2019-05-24 20:10:28 +0200

    correction, now added cors

---
+++ src/main/java/com/esliceu/keep_it_safe/configuration/CorsConfiguration.java
@@ -0,0 +1,18 @@
+package com.esliceu.keep_it_safe.configuration;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class CorsConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry corsRegistry){
+        corsRegistry.addMapping("/**");
+    }
+
+}
