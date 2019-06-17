63f478e6cc402f148e1cf1a24bfd44aecc079135
Biel Borras Serra <bielet40@gmail.com>
2019-06-17 16:55:12 +0200

    conflicts

--- src/main/java/com/esliceu/keep_it_safe/repository/InvoiceRepository.java
+++ src/main/java/com/esliceu/keep_it_safe/repository/InvoiceRepository.java
@@ -12,9 +12,6 @@ public interface InvoiceRepository extends CrudRepository<Invoice, Integer> {
 
     List<Invoice> findByUser(User user);
 
-    // @Query("from Invoice r INNER JOIN fetch r.user where r.user.user_id = :id")
-    // @EntityGraph(value = "Invoice.user", type = EntityGraph.EntityGraphType.LOAD)
-
     List<Invoice> findByuser_userId(long user_id);
 
 }
