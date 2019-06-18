0152ecf4b56b0ff9f40550ca6b745cfc97a4db30
Biel Borras Serra <bielet40@gmail.com>
2019-06-19 00:25:33 +0200

    conctant change

--- src/main/java/com/esliceu/keep_it_safe/Constants.java
+++ src/main/java/com/esliceu/keep_it_safe/Constants.java
@@ -8,4 +8,6 @@ public class Constants {
     public static final String GOOGLE_VERIFICATIONTOKEN_URL = "https://www.googleapis.com:443/plus/v1/people/me?access_token=";
     public static final int EXPIRATION_TOKEN = 3600000;
     public static final int STOCK = 100;
+    public static final String GOOGLE_CLIENT_ID = "637594007727-3o8tnk0vhafhh7o0p3hk5tib3q5rudk6.apps.googleusercontent.com";
+    public static final String GOOGLE_SECRET_ID = "kgEDC-HtqsjxQZi6Jv-kYivr";
 }
